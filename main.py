fc33271efae63dd67e07f922a2fbff65443aabe0
serge_aristide_pare <52253050+serge-aristide-pare@users.noreply.github.com>
2023-12-26 09:19:24 +0000

    Update main.py
    Version_2

--- main.py
+++ main.py
@@ -1,51 +1,43 @@
 # Importing Necessary modules
 from fastapi import FastAPI, Request
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 # Chargement du modèle
 import joblib
 from joblib import load
 import uvicorn
-import pandas as pd
 # Recuperation de l'identifiant du client
 from pydantic import BaseModel
 from typing import List
 from fastapi.encoders import jsonable_encoder
+import shap
  
 # Declaring our FastAPI instance
 app = FastAPI(
     title="Credit Scoring API",
-    description="Un simple API utilisant lemodèle de machine learning pour predire le score credit",
+    description="Un simple API utilisant le modèle de machine learning pour predire le score credit",
     version="0.1",
     )
 
-# Load model a serialized .joblib file
+base_200_clients = pd.read_csv("base_200_clients.csv", index_col='SK_ID_CURR')
 
 loaded_model = load('modele_ok.joblib')
 loaded_interpretabilite = load('interpretabilite.joblib')
-
+explainer = load('explainer.joblib')
+shap_val_global = explainer.shap_values(base_200_clients.drop(columns=['NAME_CONTRACT_TYPE']))
 cols= loaded_model.best_estimator_.named_steps['lgbm'].feature_name_
 
-
 ## API ENPOINTS
-# ------------------------------------------------------------
+# -----------------------#
 
 #########################
 # Defining path operation for root endpoint
 @app.get('/Loan application scoring dashboard')
 def main():
-    return {'message': 'This is credit scoring !'}
+    return {'message': 'Bienvenu dans notre tableau de bord du credit scoring !'}
  
-# Defining path operation for /name endpoint
-@app.get('/{name}')
-def hello_name(name : str): 
-    # Defining a function that takes only string as input and output the
-    # following message. 
-    return {'message': f'Welcome to GeeksforGeeks!, {name}'}
-
-
-
-base_200_clients = pd.read_csv("base_200_clients.csv", index_col='SK_ID_CURR')
-
-
 class request_body(BaseModel):
     id_client : int
     
@@ -61,39 +53,32 @@ def predict(data : request_body):
     else :
         prob = class_proba[0][1]
     return {"client_id": test_data, "prediction": int(class_idx), "probabilite": round(prob, 3)}
-    
 
-## Retour d'image ou valeur dans l'interpretabilite
+@app.get('/shaplocal/{client_id}')
+def shap_values_local(client_id: int):
+    """ Calcul les shap values pour un client.
+        :param: client_id (int)
+        :return: shap values du client (json).
+        """
+    client_data = base_200_clients.loc[[int(client_id)]].drop(columns=['NAME_CONTRACT_TYPE'])
+    shap_val = explainer(client_data)[0][:, 1]
+    return {'shap_values': shap_val.values.tolist(),
+            'base_value': shap_val.base_values,
+            'data': client_data.values.tolist(),
+            'feature_names': client_data.columns.tolist()}
 
-@app.get('/interpretability/{client_id}')
-def get_interpretability(client_id: int):
-    test_data = data.id_client
-    df_client = base_200_clients[base_200_clients['SK_ID_CURR']]
-    class_idx = loaded_interpretabilite.predict(df_client)[0]
-    class_proba = loaded_interpretabilite.predict_proba(df_client)
-    
-    class_prob = None
-    if df_client == 0 :
-        class_prob = 'bon client'
-    else :
-        class_prob = 'mauvais client'
-    
-    return { "client_id": client_id, "interpretability_info": class_prob }
+@app.get('/shap')
+def shap_values():
+    """ Calcul les shap values de l'ensemble du jeu de données
+    :param:
+    :return: shap values
+    """
+    client_data = base_200_clients.drop(columns=['NAME_CONTRACT_TYPE'])
+    return {'shap_values_0': shap_val_global[0].tolist(),
+            'shap_values_1': shap_val_global[1].tolist(),
+           'feature_names': client_data.columns.tolist()}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
-    #chargement des données Test
-    df_test = pd.read_csv("base_200_clients.csv", index_col='SK_ID_CURR')
-    # df_test = pd.read_csv('df_test.csv').drop('Unnamed: 0', axis = 1)
-    df_test = df_test.set_index('SK_ID_CURR')
-    #chargement du meilleur modèle
-    loaded_model = load('modele_ok.joblib')
-    loaded_interpretabilite = load('interpretabilite.joblib')
-    
-# uvicorn app:app --reload
-
-# Supprimer la colonne target dans les 200 clients avant export
-# Rajouter un autre point de sortie pour retourner l'interpretabilité du client
-    
+    uvicorn.run(app, host="0.0.0.0")
     
