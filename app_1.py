a21732f039233cf231c7053a33e98e8f683cbf46
serge_aristide_pare <52253050+serge-aristide-pare@users.noreply.github.com>
2023-12-26 09:17:13 +0000

    Update app_1.py
    Version_2

--- app_1.py
+++ app_1.py
@@ -10,16 +10,26 @@ from typing import List
 from PIL import Image
 import requests
 import plotly.graph_objects as go
+import shap
+import json
+import seaborn as sb
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from PIL import Image
+# Pour envoyer un mail
+import smtplib
+from email.mime.text import MIMEText
 
-# loaded_model = load('modele_ok.joblib')
-# loaded_interpretabilite = load('interpretabilite.joblib')
+#chargement des données Valid/Test
+df_valid = pd.read_csv("base_200_clients.csv")
+list_id = list(df_valid['SK_ID_CURR'])
+list_col = df_valid.columns.tolist()
+df_valid = df_valid.set_index('SK_ID_CURR')
+feats = [f for f in df_valid.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
+X_valid = df_valid[feats]
 
-base_200_clients = pd.read_csv("base_200_clients.csv")
-print(list(base_200_clients.columns))
-list_id=list(base_200_clients['SK_ID_CURR'])
-region = base_200_clients['REGION']
-df_test = pd.read_csv("base_200_clients.csv")
 
+
+## Début du dashboard
 st.set_page_config(page_title='Loan application scoring dashboard', 
                    page_icon = "🏡", layout="wide")
 
@@ -34,40 +44,32 @@ st.markdown("""
             </style> """, unsafe_allow_html=True)
 
 url = 'http://127.0.0.1:8000/predict'
+urli = 'http://127.0.0.1:8000/shaplocal'
+url3 = 'http://127.0.0.1:8000/shap'
 
-#chargement des données Valid/Test
-df_valid = pd.read_csv("base_200_clients.csv")
-df_valid = df_valid.set_index('SK_ID_CURR')
-feats = [f for f in df_valid.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
-X_valid = df_valid[feats]
-
-
-#chargement du palier de probabilité
-# f = open('LR_params.json')
-# thres_dict = json.load(f)
-# thres = thres_dict['Threshold']
-thres = 0.5 # a verifier et mettre la bonne valeur
-
-#Titre de la page
+##########################################################
+####### Titre de la page ################################
+##########################################################
 st.title("Projet 7 - Implémentez un modèle de scoring")
+########################################################
 
-#Menu déroulant
+# Menu déroulant
 values = list_id
 values.insert(0, '<Select>')
 num = st.sidebar.selectbox(
-    "Veuillez sélectionner un numéro de demande de prêt",
+    "**Veuillez sélectionner un numéro de demande de prêt**",
     values
 )
 if (num != '<Select>') :
     idx = num
     j_idx = {'id_client':idx}
-    # j_idx = json.dumps(idx)
     headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
     request = requests.post(url, json = j_idx, headers = headers)
     req = request.json()
-    print(req)
+    # print(req)
     proba_api = req['probabilite']
     rep_api = req['prediction']
+    # best_threshold =0.828997
 
     #Prédiction
     if rep_api == 0:
@@ -75,32 +77,30 @@ if (num != '<Select>') :
     else : 
         t = "<span class='highlight red'><span class='bold'>Rejetée</span></span>"
     
-    #Présentation des résultats
+    proba_0 = round(1-proba_api, 2)
+
+## Interpretabilité        
+    ###############################
+    # Présentation des résultats #
+    ###############################
     st.markdown(
         """
         <style>
-        .header-style {
-            font-size:25px;
-            font-family:sans-serif;
-        }
+        .header-style { font-size:25px; font-family:sans-serif;}
         </style>
-        """,
-        unsafe_allow_html = True
+        """, unsafe_allow_html = True
     )
     st.markdown(
         """
         <style>
-        .font-style {
-            font-size:20px;
-            font-family:sans-serif;
-        }
+        .font-style { font-size:20px; font-family:sans-serif; }
         </style>
         """, unsafe_allow_html = True )
-    st.markdown('<h2> Statut de la demande </h2>', unsafe_allow_html = True)
+    # st.markdown("<h2> Statut de la demande n° </h2>", num #df_valid.loc[[num]]               )
+    st.write("Statut de la demande n°",num )
     column_1, column_2 = st.columns(2)
     column_1.markdown('<h3>Décision</h3>', unsafe_allow_html = True)
     column_1.markdown(t, unsafe_allow_html = True)
-
     column_2.markdown('<h3>Score</h3>', unsafe_allow_html = True)
     column_2.subheader(f"{proba_api}")
     
@@ -110,50 +110,68 @@ if (num != '<Select>') :
     
     gauge = go.Figure(go.Indicator(
         domain = {'x': [0, 1], 'y': [0, 1]},
-        value = proba_api,
+        value = proba_api*100,
         mode = "gauge+number",
-        title = {'text': "Score", 'font': {'size': 24}},
-        gauge = {'axis': {'range': [None, 1]},
-                 'bar': {'color': "grey"},
+        title = {'text': "Jauge de Score", 'font': {'size': 24}},
+        gauge = {'axis': {'range': [None, 100], 'tickwidth': 1, 'tickcolor': "black"},
+                 'bar': {'color': "blue"},
                  'steps' : [
-                     {'range': [0, 0.67], 'color': "lightblue"},
-                     {'range': [0.67, 1], 'color': "lightcoral"}],
+                     {'range': [0, 20], 'color': "red"},
+                     {'range': [20, 50], 'color': "orange"},
+                     {'range': [50, 70], 'color': "lightgreen"},
+                     {'range': [70, 100], 'color': "green"}],
                  'threshold' :
-                     {'line': {'color': "red", 'width': 4}, 
-                      'thickness': 1, 'value': thres,
-                     }
+                 {'line': {'color': "red", 'width': 4}, 'thickness': 1, 'value': proba_api*100,}
                 }
             ))
-
     st.plotly_chart(gauge)
+    
+    ###############################################################
+    
+        ## Pretraitement des données
+    #***************************************************#
+    def prepocessing_var(df, scaler):
+        cols = df.select_dtypes(['float64']).columns
+        df_scaled = df.copy()
+        if scaler == 'minmax':
+            scal = MinMaxScaler()
+        else:
+            scal = StandardScaler()
 
-#     # Graphique camembert
-#     st.sidebar.markdown("<u>Différence solvabilité / non solvabilité</u>", unsafe_allow_html=True)
+        df_scaled[cols] = scal.fit_transform(df[cols])
+        return df_scaled
 
-#     plt.pie(targets, explode=[0, 0.1], labels=["Solvable", "Non solvable"], autopct='%1.1f%%',
-#             shadow=True, startangle=90)
-#     st.sidebar.pyplot()
-    
-    
-    ########################
-    # Affichage état civil #
-    ########################
-    # identite_client=base_200_clients
-    st.header("**Informations client**")
-    #infos = st.checkbox("Afficher les informations du client?")
+    ## Interpretation locale
+    #***************************************************#
+    def valeur_shape(num):
+        url_get_shap_local = urli+"/"+str(num)
+        response = requests.get(url_get_shap_local)
+        res = response.json()
+        print(res)
+        shap_val_local = res['shap_values']
+        base_value = res['base_value']
+        feat_values = res['data']
+        feat_names = res['feature_names']
 
-    if st.checkbox("Afficher les informations du client?"):
-        
-        
-        #st.write("Age client :", infos_client["age"], "ans.")
-        st.write("Statut id_client :**", num, "**")
-        st.write("Montant credit :**", df_valid.loc[[num]]["AMT_CREDIT"].values[0], "**")
-        # st.write("Nombre d'enfant(s) :**", base_200_clients["CNT_CHILDREN"][0], "**")
-        # st.write("Age client :", int(base_200_clients["DAYS_BIRTH"].values / -365), "ans.")
-        # st.write("Ancienneté service :", int(base_200_clients["DAYS_EMPLOYED"].values / -365), "ans.")
-        # st.write("Ancienneté client :", int(base_200_clients["DAYS_REGISTRATION"].values / -365), "ans.")
-    
-    
+        explanation = shap.Explanation(np.reshape(np.array(shap_val_local, dtype='float'), 
+                                                  (1, -1)), base_value,
+                                       data=np.reshape(np.array(feat_values, dtype='float'), 
+                                                       (1, -1)), feature_names=feat_names)
+        return explanation[0]
+
+    ## Interpretation globale
+    #***************************************************#
+    def get_shap_val():
+        url_get_shap = url3
+        response = requests.get(url_get_shap)
+        content = response.json()
+        shap_val_glob_0 = content['shap_values_0']
+        shap_val_glob_1 = content['shap_values_1']
+        shap_globales = np.array([shap_val_glob_0, shap_val_glob_1])
+
+        return shap_globales, content['feature_names']
+    ###############################################################
+
     ############################
     #Importance des variables #
     ###########################
@@ -179,165 +197,129 @@ if (num != '<Select>') :
             """,
             unsafe_allow_html = True
     )
-    nb_ = st.slider('Variables à visualiser', 0, 25, 10)
-    if 'key' not in st.session_state:
-        st.session_state['nb_var'] = nb_
-    else : 
-        st.session_state['nb_var'] = nb_
-            
-    nb = st.session_state['nb_var']
 
     column_1, column_2 = st.columns(2)
-    #Détails individuels
-    column_1.markdown(
-            '<h3>Détails individuels</h3>',
-            unsafe_allow_html = True
-    )
     
-#     coef_loc = feat_loc(idx, df_test, best_model, explainer, nb)
-#     column_1.plotly_chart(coef_loc, use_container_width = True)
-#     #Détails globaux
-#     column_2.markdown(
-#             '<h3>Détails globaux</h3>',
-#             unsafe_allow_html = True
-#     )
-#     coef_glo = feat_glo(coef_global, nb)
-#     column_2.plotly_chart(coef_glo, use_container_width = True)
+    # Détails individuels
+    with column_2 :
         
-#     #Choix de la variables à expliquée
-#     feats = df_desc['Row'].tolist()
-#     var = st.selectbox(
-#             "Veuillez sélectionner une variable à définir",
-#             feats
-#     )
-#     d = df_desc.loc[df_desc['Row'] == var]
-#     st.write('Variable {} : {}'.format(var, d['Description'].values[0]))
-    
-    
-    
-    
-#     #Bouton pour voir distribution
-    st.markdown(
-            """
-            <style>
-            .header-style {
-                font-size:25px;
-                font-family:sans-serif;
-            }
-            </style>
-            """,
-            unsafe_allow_html = True
-    )
-    st.markdown(
-            """
-            <style>
-            .font-style {
-                font-size:20px;
-                font-family:sans-serif;
-            }
-            </style>
-            """,
-            unsafe_allow_html = True
-    )
-
-    if st.button('Voir distribution par classes'):
-        st.markdown(
-        '<h2> Distribution des probabilités </h2>',
-        unsafe_allow_html = True
+        column_2.markdown(
+                '<h3>Détails individuels</h3>',
+                unsafe_allow_html = True
         )
-        hist = dist_proba(X_valid, best_model, proba_api)
-        st.plotly_chart(hist)    
-    
+        headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 
-# # Prediction
-# if st.sidebar.button ("Predire"):
-#     prediction, probability = make_prediction(input_data)
-#     st.subheader("Probabilités :")
-#     prob_df = pd.DataFrame({
-#         'Categories': ["No Default", "Default"],
-#         'Probabilité': probability[0],
-#     })
-    
-#     fig = px.bar(prob_df, x='Catégories', y = 'Probabilité', labels={'Probabilité': 'Probabilité (%)'})
-#     st.plotly_chart(fig)
-    
-#     st.subheader("Résultat de prediction")
-#     if prediction[0] ==1:
-#         st.error("le client en defaut")
-#     else:
-#         st.succes("le clientne sera pas en defaut")
+        st.info("Interprétation locale de la prédiction")
+        shap_val = valeur_shape(num)
+        nb_features = st.slider('Nombre de variables à visualiser', 0, 20, 11)
+        # Affichage du waterfall plot : shap local
+        fig = shap.waterfall_plot(shap_val, max_display=nb_features, show=False)
+        st.pyplot(fig)
 
+        with st.expander("Explication du graphique", expanded=False):
+            st.caption("Ici sont affichées les caractéristiques influençant de manière locale la décision. "
+                           "C'est-à-dire que ce sont les caractéristiques qui ont influençé la décision pour ce client "
+                           "en particulier.")
+    
+    # Détails globaux
+    with column_1 :
+        column_1.markdown(
+                '<h3>Détails globaux</h3>',
+                unsafe_allow_html = True
+        )
+        st.info("Importance globale")
+        n_features = st.slider('Nombre de variables à visualiser', 0, 20, 9)
+        shap_values, feature_names = get_shap_val()
+        fig, ax = plt.subplots()
+        # Affichage du summary plot : shap global
+        ax = shap.summary_plot(shap_values[1], shap_values[0], plot_type='bar', max_display=n_features, feature_names=feature_names)
+        st.pyplot(fig)
 
-# # Ajouter une barre de défilement pour ajuster le nombre de points de données
-# # num_points = st.slider("Nombre de points de données", min_value=10, max_value=1000, value=100)
-# # st.markdown(f"Vous avez choisi {list_id} points de données.")
+        with st.expander("Explication du graphique", expanded=False):
+            st.caption("Ici sont affichées les caractéristiques influençant de manière globale la décision.")  
+    
+##############################################
+# Affichage information sur état civil ######
+#############################################
 
-# def show_predict_page():
-#     # st.title("credit scoring")
+if st.checkbox("Afficher les informations supplémentaires du client?"):
+    st.header("**Informations client**")
+    st.write("Identifiant du client :", num)
+    st.write("Age du client :", int(df_valid.loc[[num]]["DAYS_BIRTH"].values[0] / -365), "ans.")
+    st.write("Montant credit :", df_valid.loc[[num]]["AMT_CREDIT"].values[0])
+    st.write("Nombre d'enfant(s) :", df_valid.loc[[num]]["CNT_CHILDREN"].values[0])
+    st.write("Ancienneté du client à la banque :", int(df_valid.loc[[num]]["DAYS_REGISTRATION"].values[0] / -365), "ans.")
     
-#     st.write("""### Please enter the requiered information to predict cerdit scoring""")
-    
-# #     id_client = list_id #('127633', '251751') #list_id
-    
-# #     id_clients=st.selectbox("id_clients", list_id)
-    
-    
-#     # ok = st.button("credi scor")      
+#############################################    
+#### Histogramme de quelques variables ######
+#############################################
+values_col = list_col
+values_col.insert(0, '<Select>')
+col = st.sidebar.multiselect(
+    # st.multiselect
+    "**Veuillez sélectionner une variable dans la base de demande de prêt**",
+    values_col
+)
+if (col != '<Select>') :
+    # Créer un histogramme pour la colonne sélectionnée
+    for col in col:
+        st.header("**Voici la distribution des variables que vous avez selectionner**")
+        # print(df_valid[col])
+        st.header(f"*Voici la distribution de la variable {col}*")
         
-
-#     # st.subheader(f"La proba du client est ${make_prediction:.2f}")
+        # Generate an histogram for the current variable
+        plt.figure(figsize = (8,6))
+        sb.histplot(df_valid[col], kde=False)
         
-# show_predict_page()
-
-
-
-
-
-
-# # Creer un dossier dashboard comme avec l'appli main
-# # charger le DF 200 clients 
-# # Crer un drop down list où on a la liste de tous les id_client
-
-# # Utiliser requests pour envoyer le numero du client à l'api afin d'avoir une reponse
-
-
-
-# # import streamlit as st
-# # import streamlit.components.v1 as components
-
-# from PIL import Image
-# import time
-
-# st.markdown("<h2 style='text-align:center; color:floralWhite;'> CREDIT SCORE USING MACHINE LEARNING</h2>", unsafe_allow_html=True)
-
-# col1, col2, col3 = st.columns([1,8,1])
-
-# try:
-#     img1 = Image.open("image.jpg")
+        # Calculate the average/mean
+        avg_line = np.mean(df_valid[col])
+        
+        #Add a vertical line for the average
+        plt.axvline(avg_line, color='red', linestyle='dashed', linewidth=2, label='Moyenne')
+        plt.title( f"Distribution de : {col}")
+        plt.xlabel(col)
+        plt.ylabel("Count")
+        
+        #Show the plot
+        plt.legend() # ajout legende avec moyenne
+        plt.show()  
+        st.pyplot(plt)
+    st.markdown('[A Project by L. Serge Aristide PARE](https://github.com/serge-aristide-pare)')
     
+# Contact Form
+with st.expander("Contact us"):  
 
-#     with col2:
-#         st.image(img1, caption = "Credit Risk Analysis")
-#         st.markdown('[A Project by L. Serge Aristide PARE](https://github.com/serge-aristide-pare)')
+    st.title('Send Email 💌 🚀')
 
-# except:
-#     components.html('''
-#     <script>
-#         alert("Image Not Loading")
-#     </script>
-#     ''')
-#     st.text("Image Not Loading")
+    st.markdown("""
+    *Enter your email, subject, and email body then hit send to receive an email!*
+    """)
 
-# else:
-#     pass
+    # Taking inputs
+    email_sender = st.text_input('From', 'Veuillez mettre votre mail')
+    email_receiver = st.text_input('To')
+    subject = st.text_input('Subject')
+    body = st.text_area('Body')
 
-# finally:
-#     pass
+    # Password input
+    password = st.text_input('Password', type="password")
 
-# # Contact Form
+    if st.button("Send Email"):
+        try:
+            msg = MIMEText(body)
+            msg['From'] = email_sender
+            msg['To'] = email_receiver
+            msg['Subject'] = subject
 
-# with st.expander("Contact us"):
-#     with st.form(key='contact', clear_on_submit=True):
-#         email = st.text_input('Contact Email')
-#         st.text_area("Query","Please fill in all the information or we may not be able to process your request")
-#         submit_button = st.form_submit_button(label='Send Information')
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+
+            # Utiliser le mot de passe d'application ou le mot de passe principal
+            server.login(email_sender, password)
+
+            server.sendmail(email_sender, email_receiver, msg.as_string())
+            server.quit()
+
+            st.success('Email sent successfully! 🚀')
+        except Exception as e:
+            st.error(f"Failed to send email: {e}")
